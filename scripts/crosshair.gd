487fc7151b4b9b00a0dd6426fa2def8ef0e42b1e
Garrett59100 <garrett59100@gmail.com>
2024-11-02 17:08:27 -0400

    more crosshair implementation

--- scripts/crosshair.gd
+++ scripts/crosshair.gd
@@ -1,26 +1,26 @@
-extends StaticBody2D
+class_name Crosshair
+extends Area2D
 
 const SPEED = 300.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _object_entered(body: Node2D):
+	if Input.is_action_just_pressed("fire"):
+		# kill the duck
+		if body.get_class() == "Duck":
+			body.queue_free()
+			print(body)
 
+func _ready():
+	monitoring = true
+	body_entered.connect(_object_entered)
 
 func _process(delta):
-	# Handle jump.
+	scale = Vector2(1, 1)
 	if Input.is_action_just_pressed("fire"):
-		pass
+		scale = Vector2(0.9, 0.9)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	if Input.is_action_pressed("move_left"):
-		pass
-
-	var ver_direction = Input.get_axis("move_down", "move_up")
+	var ver_direction = Input.get_axis("move_up", "move_down")
 	var hor_direction = Input.get_axis("move_left", "move_right")
-
-	print(ver_direction)
-	print(hor_direction)
 
 	move_local_y(SPEED * delta * ver_direction)
 	move_local_x(SPEED * delta * hor_direction)
