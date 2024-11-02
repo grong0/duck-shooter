b6f5815d7589ebaddded50fea8bb78aeba6f50e7
Garrett59100 <garrett59100@gmail.com>
2024-11-02 17:40:30 -0400

    crosshair now kills ducks :D

--- scripts/crosshair.gd
+++ scripts/crosshair.gd
@@ -3,21 +3,13 @@ extends Area2D
 
 const SPEED = 300.0
 
-func _object_entered(body: Node2D):
-	if Input.is_action_just_pressed("fire"):
-		# kill the duck
-		if body.get_class() == "Duck":
-			body.queue_free()
-			print(body)
-
-func _ready():
-	monitoring = true
-	body_entered.connect(_object_entered)
-
 func _process(delta):
 	scale = Vector2(1, 1)
 	if Input.is_action_just_pressed("fire"):
 		scale = Vector2(0.9, 0.9)
+		for duck in get_overlapping_bodies():
+			duck.queue_free()
+			print(duck)
 
 	var ver_direction = Input.get_axis("move_up", "move_down")
 	var hor_direction = Input.get_axis("move_left", "move_right")
