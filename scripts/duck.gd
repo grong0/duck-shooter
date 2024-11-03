64af631de7eca5dd172db234d9886bb70d76894d
Dylan Shaffer <swimdoctor5@gmail.com>
2024-11-02 20:08:28 -0400

    Added duck sprites and finished duck class

--- scripts/duck.gd
+++ scripts/duck.gd
@@ -5,22 +5,32 @@ enum DuckType {
 	Yellow,
 	Green,
 	Pink,
-	Golden,
-	Pirate
+	Golden
 }
 
-const my_scene: PackedScene = preload("res://duck.tscn")
+static var duckDictionary: Dictionary = {
+	DuckType.Yellow : {"speed":1, "points":10,  "sprite":"YellowDuck"},
+	DuckType.Green :  {"speed":2, "points":20,  "sprite":"GreenDuck"},
+	DuckType.Pink :   {"speed":4, "points":40,  "sprite":"PinkDuck"},
+	DuckType.Golden : {"speed":4, "points":100, "sprite":"GoldenDuck"}
+}
+
+static var my_scene: PackedScene
 const SPEED = 100.0
 
 var speed_mult: float
 var direction: int
+var points: int
 
 static func Duck(duckType: DuckType, direction := 1) -> Duck:
+	if(my_scene == null):
+		my_scene = load("res://duck.tscn")
 	var new_duck: Duck = my_scene.instantiate()
 	new_duck.direction = direction
-	match(duckType):
-		_:
-			new_duck.speed_mult = 1
+	new_duck.speed_mult = duckDictionary[duckType]["speed"]
+	new_duck.points = duckDictionary[duckType]["points"]
+	var sprite: Sprite2D = new_duck.get_child(0)
+	sprite.texture = load("res://Sprites/" + duckDictionary[duckType]["sprite"] + ".png")
 	return new_duck
 
 func _process(delta):
